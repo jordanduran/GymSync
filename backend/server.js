c32eec07a8fab6ecc649ffa5aa9682b76c536135
Jordan Duran <jordanduranm@gmail.com>
2024-02-03 23:48:46 -0500

    Installed mongoose and express to connect to db

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const express = require('express');
+const mongoose = require('mongoose');
 const workoutRoutes = require('./routes/workouts');
 // express app
 const app = express();
@@ -17,7 +18,15 @@ app.use((req, res, next) => {
 // routes
 app.use('/api/workouts', workoutRoutes);
 
-// listen for requests
-app.listen(process.env.PORT, () => {
-  console.log('Listening on port 4000');
-});
+// connect to db
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    // listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log('Connected to MongoDB & listening on port 4000');
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
