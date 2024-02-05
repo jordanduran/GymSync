a42b4714cfaebb9124be451dd94d5333a769db38
Jordan Duran <jordanduranm@gmail.com>
2024-02-05 12:53:18 -0500

    Created controllers folder and added controls for get all, get single, and creating workout

---
+++ backend/controllers/workoutController.js
@@ -0,0 +1,47 @@
+const Workout = require('../models/workoutModel');
+const mongoose = require('mongoose');
+
+// GET all workouts
+const getAllWorkouts = async (req, res) => {
+  const workouts = await Workout.find({}).sort({ createdAt: -1 });
+  res.status(200).json(workouts);
+};
+
+// GET a single workout
+const getWorkout = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: 'Workout does not exist' });
+  }
+
+  const workout = await Workout.findById(id);
+  if (!workout) {
+    return res.status(404).json({ error: 'Workout does not exist' });
+  }
+
+  res.status(200).json(workout);
+};
+
+// CREATE new workout
+const createWorkout = async (req, res) => {
+  const { title, load, reps } = req.body;
+
+  // Add doc to DB
+  try {
+    const workout = await Workout.create({ title, load, reps });
+    res.status(200).json(workout);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+// DELETE a workout
+
+// UPDATE a workout
+
+module.exports = {
+  getAllWorkouts,
+  getWorkout,
+  createWorkout,
+};
